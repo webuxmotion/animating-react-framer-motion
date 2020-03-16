e10614c3dcb2e11af03c0fb5985e61e51338aa94
Pereverziev Andrii <pereverziev.andrii@gmail.com>
2020-03-16 13:29:22 +0200

    Modal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { Card, CardGrid, Container, Header } from "./Elements";
+import Modal from './Modal';
 import "./App.css";
 import Menu from "./Menu";
 import blue from "./blue.png";
@@ -10,7 +11,7 @@ import green from "./green.png";
 
 function App() {
   const [ value, setValue ] = useState(0)
-  const [isToggled, setToggle] = useState(1)
+  const [isToggled, setToggle] = useState(false)
 
   return (
     <motion.div
@@ -29,16 +30,9 @@ function App() {
         <h1>Header</h1>
       </Header>
       <Container>
-        <motion.h2
-          animate={{
-            x: value + "px",
-            opacity: isToggled
-          }}
-        >Super Cool</motion.h2>
+        <h2>Super Cool</h2>
         <button
-          onClick={() => setToggle((prevValue) => {
-            return prevValue ? 0 : 1
-          })}
+          onClick={() => setToggle(true)}
         >Toggle</button>
         <p>{value}</p>
         <input 
@@ -48,6 +42,12 @@ function App() {
           value={value} 
           onChange={e => setValue(e.target.value)}
         />
+        <Modal isToggled={isToggled} setToggle={setToggle}>
+          <Card style={{ background: "var(--purp)" }}>
+            <h3>Some card</h3>
+            <img src={purp} />
+          </Card>
+        </Modal>
         <CardGrid>
           <Card style={{ background: "var(--purp)" }}>
             <h3>Some card</h3>
